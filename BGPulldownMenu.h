4e063b17c63adf2d2297d297070a4378e777e0a5
Matthew Smith <m@lattejed.com>
2014-07-14 13:01:53 +0700

    Misc refactoring

--- BGPulldownMenu.h
+++ BGPulldownMenu.h
@@ -1,17 +1,40 @@
 //
-//  PulldownMenu.h
+//  BGPulldownMenu.h
 //
 //  Created by Bernard Gatt
 //
 
+/*
+The MIT License (MIT)
+
+Copyright (c) 2013 Bernard Gatt
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #import <UIKit/UIKit.h>
 
-@protocol PulldownMenuDelegate
+@protocol BGPulldownMenuDelegate
     -(void)menuItemSelected:(NSIndexPath *)indexPath;
     -(void)pullDownAnimated:(BOOL)open;
 @end
 
-@interface PulldownMenu : UIView<UITableViewDataSource, UITableViewDelegate> {
+@interface BGPulldownMenu : UIView<UITableViewDataSource, UITableViewDelegate> {
     UITableView *menuList;
     NSMutableArray *menuItems;
     
@@ -26,7 +49,7 @@
     float tableHeight;
 }
 
-@property (nonatomic, assign) id<PulldownMenuDelegate> delegate;
+@property (nonatomic, assign) id<BGPulldownMenuDelegate> delegate;
 @property (nonatomic, retain) UITableView *menuList;
 @property (nonatomic, retain) UIView *handle;
 
